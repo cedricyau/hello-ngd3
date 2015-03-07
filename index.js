5efc4e0041c3a021ea0948a8d609cac875d6e282
Cedric Yau <cedric@broadwaycio.com>
2015-03-07 14:56:37 -0500

    re-render chart

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ angular.module('myApp', []).
      //explicitly creating a directive definition variable
      //this may look verbose but is good for clarification purposes
      //in real life you'd want to simply return the object {...}
-     var directiveDefinitionObject = {
+     return {
          //We restrict its use to an element
          //as usually  <bars-chart> is semantically
          //more understandable
@@ -13,41 +13,52 @@ angular.module('myApp', []).
          //this is important,
          //we don't want to overwrite our directive declaration
          //in the HTML mark-up
-         replace: false,
+         replace: true,
          //our data source would be an array
          //passed thru chart-data attribute
          scope: {data: '=chartData'},
          link: function (scope, element, attrs) {
-           //in D3, any selection[0] contains the group
-           //selection[0][0] is the DOM node
-           //but we won't need that this time
-           var chart = d3.select(element[0]);
-           //to our original directive markup bars-chart
-           //we add a div with out chart stling and bind each
-           //data entry to the chart
-            chart.append("div").attr("class", "chart")
-             .selectAll('div')
-             .data(scope.data).enter().append("div")
-             .transition().ease("elastic")
-             .style("width", function(d) { return d + "%"; })
-             .text(function(d) { return d + "%"; });
-           //a little of magic: setting it's width based
-           //on the data value (d) 
-           //and text all with a smooth transition
+             scope.render = function(data) {
+               //in D3, any selection[0] contains the group
+               //selection[0][0] is the DOM node
+               //but we won't need that this time
+               var chart = d3.select(element[0]);
+               chart.selectAll('div').remove();
+               //to our original directive markup bars-chart
+               //we add a div with out chart stling and bind each
+               //data entry to the chart
+                chart.append("div").attr("class", "chart")
+                 .selectAll('div')
+                 .data(data).enter().append("div")
+                 .transition().ease("elastic")
+                 .style("width", function(d) { return d + "%"; })
+                 .text(function(d) { return d + "%"; });
+               //a little of magic: setting it's width based
+               //on the data value (d) 
+               //and text all with a smooth transition
+             };
+
+            scope.$watch('data', function() {
+                console.log('data has changed');
+                console.log(scope.data);
+                scope.render(scope.data);
+            });
 
          } 
       };
       
-      return directiveDefinitionObject;
    }).
     controller('Ctrl', ['$scope', '$http', function($scope, $http) {
-        $scope.myData = [10,20,30,40,60];
+        $scope.initialize = function () {
+            $scope.chartData = [10,20,30,40,60];
+        };
+
 
         $scope.loadData = function() {
             var httpRequest = $http.jsonp('https://sly.01phi.com:53850/echo?callback=JSON_CALLBACK',
                 {params: {"key1": "value1", "key2": "value2"}}).
             success(function(data, status, headers, config) {
-                $scope.myData = data;
+                $scope.chartData = data;
                 console.log(data);
             }).
             error(function(data, status, headers, config) {
@@ -56,5 +67,7 @@ angular.module('myApp', []).
                 console.log(data);
             });
         };
+
+        $scope.initialize();
     }]);
 
